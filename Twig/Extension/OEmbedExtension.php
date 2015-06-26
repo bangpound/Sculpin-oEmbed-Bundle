faf05d4abf37991b96d2c568e16da2d1999cda8a
Benjamin Doherty <bdoherty@caxy.com>
2015-06-26 14:24:12 -0500

    update to guzzle 6.

--- Twig/Extension/OEmbedExtension.php
+++ Twig/Extension/OEmbedExtension.php
@@ -2,29 +2,11 @@
 
 namespace Bangpound\Sculpin\Bundle\OEmbedBundle\Twig\Extension;
 
-use Doctrine\Common\Cache\FilesystemCache;
-use Guzzle\Cache\DoctrineCacheAdapter;
-use Guzzle\Http\Client;
-use Guzzle\Http\Exception\BadResponseException;
-use Guzzle\Http\Message\RequestInterface;
-use Guzzle\Http\QueryString;
-use Guzzle\Plugin\Cache\CachePlugin;
-use Guzzle\Plugin\Cache\CallbackCanCacheStrategy;
-use Guzzle\Plugin\Cache\DefaultCacheStorage;
-use Guzzle\Plugin\Cache\SkipRevalidation;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
 
 class OEmbedExtension extends \Twig_Extension
 {
-    private $cache_dir;
-
-    /**
-     *
-     */
-    public function __construct($cache_dir)
-    {
-        $this->cache_dir = $cache_dir;
-    }
-
     /**
      * Returns a list of functions to add to the existing list.
      *
@@ -40,36 +22,19 @@ class OEmbedExtension extends \Twig_Extension
     public function getOEmbed($url, $params = array())
     {
         $params['url'] = $url;
+
         $client = new Client();
 
-        $dir = $this->cache_dir.'/oembed';
-        $plugin = new CachePlugin(
-            array(
-                'storage' => new DefaultCacheStorage(
-                        new DoctrineCacheAdapter(
-                            new FilesystemCache($dir, 'guzzlecache.data')
-                        )
-                    ),
-                'revalidation' => new SkipRevalidation(),
-                'can_cache' => new CallbackCanCacheStrategy(
-                        function (RequestInterface $request) {
-                            return true;
-                        }
-                    ),
-            )
-        );
-        $client->addSubscriber($plugin);
-
         try {
-            $query = new QueryString($params);
-            $request = $client->get('http://api.embed.ly/1/oembed?'.$query);
-            $request->getParams()->set('cache.override_ttl', 3600);
-            $response = $request->send();
-            $data = $response->json();
+            $response = $client->get('http://api.embed.ly/1/oembed', array(
+              'query' => $params,
+            ));
+            $data = json_decode($response->getBody()->getContents(), true);
             $response = $data['html'];
 
             return $response;
         } catch (BadResponseException $e) {
+            return $url;
         }
     }
 
